e608aa3f1c8f2bc2373c72fb8762dca400983e44
SajidKhanMarwat <msajidmarwat@gmail.com>
2023-02-02 16:38:13 +0500

    work updates

---
+++ WebsiteCrawler/WebsiteCrawler/Controllers/CrawlerController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using HtmlAgilityPack;
+using WebsiteCrawler.Models;
+
+namespace WebsiteCrawler.Controllers
+{
+    public class CrawlerController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(URL inPut)
+        {
+            try
+            {
+                if (inPut.Url != "" && inPut.Url.StartsWith("http"))
+                {
+                    ViewBag.URL = inPut.Url;
+                }
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                return View(ex);
+            }
+        }
+    }
+}
---
+++ WebsiteCrawler/WebsiteCrawler/Models/URL.cs
@@ -0,0 +1,7 @@
+﻿namespace WebsiteCrawler.Models
+{
+    public class URL
+    {
+        public string Url { get; set; }
+    }
+}
